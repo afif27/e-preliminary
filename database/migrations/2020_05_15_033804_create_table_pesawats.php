48eb24301726c8b88bee2b74ff2cacfa1a8a1d8d
apip_tech <afiffadilah.tif@gmail.com>
2020-05-15 13:56:15 +0000

    menambah database pesawat, controller pesawat, routuing pesawat

---
+++ database/migrations/2020_05_15_033804_create_table_pesawats.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePesawats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesawats', function (Blueprint $table) {
+            $table->id();
+            $table->string('registasi');
+            $table->string('airlanes');
+            $table->timestamp('rts_plan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesawats');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource("users", "UserController");
+Route::resource('pesawats', 'PesawatController');
