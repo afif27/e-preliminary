d1832d5e3190656f7cabda302e938410ea6b3430
apip_tech <afiffadilah.tif@gmail.com>
2020-05-17 13:00:02 +0000

    create table prelim, belum di migrate

---
+++ database/migrations/2020_05_17_024412_create_table-prelims.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePrelims extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prelims', function (Blueprint $table){
+            $table->increments('id');
+            $table->bigInteger('aircraft_id')->unsigned()->nullable();
+            $table->foreign('aircraft_id')->references('id')->on('aircrafts');
+            $table->string('description');
+            $table->string('finding');
+            $table->string('seat_position');
+            $table->string('action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prelims');
+    }
+}
